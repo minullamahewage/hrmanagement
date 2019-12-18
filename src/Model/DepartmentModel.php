b68ec02aa18412ef3af557862ff85d586ce569b9
Minul Lamahewage <minullamahewage@gmail.com>
2019-12-18 18:32:50 +0530

    department

--- src/Model/DepartmentModel.php
+++ src/Model/DepartmentModel.php
@@ -9,6 +9,39 @@ class DepartmentModel{
         $sql = "SELECT * FROM department ORDER BY dept_id ASC";
         $stmt = $conn->prepare($sql);
         $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function addDepartment($department, $em){
+        $conn = $em->getConnection();
+        $sql = "INSERT INTO department(dept_id, dept_name, building, floor) VALUES (:dept_id, :dept_name, :building, :floor)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':dept_id', $department->getDeptId());
+        $stmt->bindValue(':dept_name',$department->getDeptName());
+        $stmt->bindValue(':building', $department->getBuilding());
+        $stmt->bindValue(':floor', $department->getFloor());
+        $stmt->execute();
+    }
+
+    public function changeDepartmentDetails($department, $em){
+        $conn = $em->getConnection();
+        $sql = "UPDATE department SET dept_id = dept_id, dept_name = :dept_name, building = :building, floor = :floor WHERE dept_id = :dept_id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':dept_id', $department->getDeptId());
+        $stmt->bindValue(':dept_name',$department->getDeptName());
+        $stmt->bindValue(':building', $department->getBuilding());
+        $stmt->bindValue(':floor', $department->getFloor());
+        $stmt->execute();
+
+    }
+
+    public function getDepartmentName($dept_id, $em){
+        $conn = $em->getConnection();
+        $sql = "SELECT dept_name FROM department WHERE dept_id = :dept_id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':dept_id', $dept_id);
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
 
 }
