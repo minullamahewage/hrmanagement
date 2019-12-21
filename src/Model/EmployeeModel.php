182010c5bff8f2238752b780248b4e86c2c43375
Minul Lamahewage <minullamahewage@gmail.com>
2019-12-21 21:52:12 +0530

    employee model1

---
+++ src/Model/EmployeeModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Model;
+
+class EmployeeModel{
+
+    // public function getAllEmployees($em){
+    //     $conn = $em->getConnection();
+    //     $sql = "SELECT * FROM employee ORDER BY n";
+    // }
+
+}
