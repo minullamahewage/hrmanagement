d09774cb18587637de19666f6e95b48841058f28
Minul Lamahewage <minullamahewage@gmail.com>
2019-12-30 19:59:33 +0530

    added report branch

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,166 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Branch;
+use App\Entity\Employee;
+use App\Entity\EmpData;
+use App\Form\ReportsForm\ReportBranchType;
+use App\Model\BranchModel;
+use App\Model\DepartmentModel;
+use App\Model\EmployeeModel;
+use App\Model\EmploymentStatusModel;
+use App\Model\EmpTelephoneModel;
+use App\Model\EmpCustomModel;
+use App\Model\EmpDataModel;
+use App\Model\JobTitleModel;
+use App\Model\PayGradeModel;
+use App\Model\ReportModel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/report")
+ */
+class ReportController extends AbstractController
+{
+
+    // //admin show all employees
+    // /**
+    //  * @Route("/admin", name="employee_index", methods={"GET"})
+    //  */
+    // public function test(): Response
+    // {
+    //     $employeeModel = new EmployeeModel();
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     $employees = $employeeModel->getAllEmployees($entityManager);
+    //     //custom attribute list
+    //     $empCustomModel = new EmpCustomModel();
+    //     $customAtrtibutes = $empCustomModel->getAllCustomAttributes($entityManager);
+
+    //     //changing job title id and emp status id to job title and emp status
+    //     $jobTitleModel = new JobTitleModel();
+    //     $empStatusModel = new EmploymentStatusModel();
+    //     $empTelephoneModel = new EmpTelephoneModel();
+    //     $empDataModel = new EmpDataModel();
+    //     foreach ($employees as &$employee){
+    //         //job title
+    //         $jobTitleId = $employee['job_title_id'];
+    //         $jobTitle = $jobTitleModel->getJobTitle($jobTitleId, $entityManager);
+    //         $employee['job_title'] = $jobTitle;
+    //         //employment status
+    //         $empStatusId = $employee['emp_status_id'];
+    //         $empStatus = $empStatusModel->getEmploymentStatus($empStatusId, $entityManager);
+    //         $employee["emp_status"] = $empStatus;
+    //         //adding employee telephone no.s to employee array
+    //         $empId = $employee['emp_id'];
+    //         $empTelephone = $empTelephoneModel->getEmpTelephone($empId, $entityManager);
+    //         $employee["emp_telephone"] = $empTelephone;
+    //         //custom attributes
+    //         $empCusAttr = $empDataModel->getDataByEmpId($empId, $entityManager);
+    //         $employee["emp_cus_attr"] = $empCusAttr;
+    //     }
+
+    //     return $this->render('employee/index.html.twig', [
+    //         'employees' => $employees,
+    //         'cus_attr' => $customAtrtibutes,
+    //     ]);
+    // }
+
+
+    /**
+     * @Route("/", name="report_index", methods={"GET","POST"})
+     */
+    public function index(Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $branch = new Branch();
+        $employee = new Employee();
+        $reportModel = new ReportModel();
+        $employeeModel = new EmployeeModel();
+        $empDataModel = new EmpDataModel();
+        $empCustomModel = new EmpCustomModel();
+        $branchModel = new BranchModel();
+        $deptModel = new DepartmentModel();
+        $jobTitleModel = new JobTitleModel();
+        $payGradeModel = new PayGradeModel();
+        $employmentStatusModel = new EmploymentStatusModel();
+        //Getting data from db for select drop down
+        //branchId
+        $branchIds = $branchModel->getAllBranches($entityManager);
+        $branchChoices;
+        foreach($branchIds as &$branch){
+            $branchChoices[$branch['branch_id'].'-'.$branch['name']] = $branch['branch_id'];
+        }
+        // //deptId
+        // $deptIds = $deptModel->getAllDepartments($entityManager);
+        // $deptChoices;
+        // foreach($deptIds as &$dept){
+        //     $deptChoices[$dept['dept_id'].'-'.$dept['dept_name']] = $dept['dept_id'];
+        // }
+        // //jobTitle
+        // $jobTitles = $jobTitleModel->getAllJobTitles($entityManager);
+        // $jobTitleChoices;
+        // foreach($jobTitles as &$jobTitle){
+        //     $jobTitleChoices[$jobTitle['job_title']] = $jobTitle['job_title'];
+        // }
+        // //payGrade
+        // $payGrades = $payGradeModel->getAllPayGrades($entityManager);
+        // $payGradeChoices;
+        // foreach($payGrades as &$payGrade){
+        //     $payGradeChoices[$payGrade['pay_grade']] = $payGrade['pay_grade'];
+        // }
+        // //employment Status
+        // $empStatuses = $employmentStatusModel->getAllEmploymentStatuses($entityManager);
+        // $empStatusChoices;
+        // foreach($empStatuses as &$empStatus){
+        //     $empStatusChoices[$empStatus['emp_status']] = $empStatus['emp_status'];
+        // }
+        $formBranch = $this->createForm(ReportBranchType::class, $branch, array(
+            'branch_choices' =>$branchChoices,
+        ));     
+        $formBranch->handleRequest($request);
+        if ($formBranch->isSubmitted() && $formBranch->isValid()) {
+            $branchId = $branch->getBranchId();
+            return $this->redirectToRoute('report_branch', array('branchId' =>$branchId));
+        }
+
+        return $this->render('report/index.html.twig', [
+            'form_branch' => $formBranch->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/branch/{branchId}", name="report_branch", methods={"GET"})
+     */
+    public function showEmpByBranch($branchId): Response
+    {
+        $employeeModel = new EmployeeModel();
+        $branchModel = new BranchModel();
+        $entityManager = $this->getDoctrine()->getManager();
+        $employees = $branchModel->getEmpByBranch($branchId, $entityManager);
+        //changing job title id and emp status id to job title and emp status
+        $jobTitleModel = new JobTitleModel();
+        $empStatusModel = new EmploymentStatusModel();
+        $empTelephoneModel = new EmpTelephoneModel();
+        $empDataModel = new EmpDataModel();
+        foreach ($employees as &$employee){
+            //job title
+            $jobTitleId = $employee['job_title_id'];
+            $jobTitle = $jobTitleModel->getJobTitle($jobTitleId, $entityManager);
+            $employee['job_title'] = $jobTitle;
+            //employment status
+            $empStatusId = $employee['emp_status_id'];
+            $empStatus = $empStatusModel->getEmploymentStatus($empStatusId, $entityManager);
+            $employee["emp_status"] = $empStatus;
+        }
+
+
+        return $this->render('report/branch.html.twig', [
+            'employees' => $employees,
+        ]);
+    }
+    
+}
---
+++ src/Form/ReportsForm/ReportBranchType.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Form\ReportsForm;
+
+use App\Entity\Branch;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReportBranchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->branchChoices = $options['branch_choices'];
+                
+        $builder
+            ->add('branchId', ChoiceType::class,[
+                'choices' => $this->branchChoices,
+            ])
+            
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Branch::class,
+            'branch_choices' =>null
+        ]);
+    }
+}
